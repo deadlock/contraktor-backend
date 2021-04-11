0fb37264f4424024d161c8ea9116ba9567ad95ce
diogo <diogo.silva@orsegups.com.br>
2021-04-11 14:25:05 -0300

    removing dist file

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const routes_1 = require("./src/config/routes");
-const server_1 = require("./src/config/server");
-const port = process.env.PORT || 8080;
-const environment = process.env.NODE_ENV || "development";
-server_1.createExpressServer(routes_1.createExpressRouter())
-    .listen(port)
-    .once("listening", () => console.info("Server running", { port, environment }));
